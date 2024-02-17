2656aa58e9c0cbb7c9660ded6797bd589adc177b
Nick Brian <nbjm1234@outlook.com>
2024-02-17 01:00:00 -0600

    improve accessibility

--- src/components/Projects/components/button/ProjectButtons.tsx
+++ src/components/Projects/components/button/ProjectButtons.tsx
@@ -3,17 +3,21 @@ import { ProjectButtonList } from "../../utils/projectsButtonsList/projectButton
 import LinkButton from "@/components/components/linkButton/LinkButton";
 export default function ProjectButtons() {
   return (
-    <div className="flex justify-between max-w-3xl m-auto mt-2 ">
-      {ProjectButtonList.map((button) => (
-        <LinkButton
-          className="sm:px-3 px-1 py-2 border-2 border-blue-400 rounded-lg text-blue-500 font-semibold"
-          target="_self"
-          key={button.title}
-          href={button.href}
-        >
-          {button.title}
-        </LinkButton>
-      ))}
-    </div>
+    <nav className="flex justify-between max-w-3xl m-auto mt-2 ">
+      <ul>
+        {ProjectButtonList.map((button) => (
+          <li key={button.title}>
+            <LinkButton
+              className="sm:px-3 px-1 py-2 border-2 border-blue-400 rounded-lg text-blue-500 font-semibold"
+              target="_self"
+              key={button.title}
+              href={button.href}
+            >
+              {button.title}
+            </LinkButton>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
